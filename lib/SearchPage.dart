e7d58f12a85c4828640e5b0aed0b2860fa5284ea
Aymen-Ziouche <aymenannaba16@gmail.com>
2021-02-20 23:58:20 +0100

    Completed SearchPage

--- lib/SearchPage.dart
+++ lib/SearchPage.dart
@@ -3,6 +3,180 @@ import 'package:flutter/material.dart';
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: Padding(
+      padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 15.0),
+      child: Column(
+        children: <Widget>[
+          Center(
+            child: Text(
+              "Search",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 48.0,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(18.0),
+            child: TextField(
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                    size: 28.0,
+                  ),
+                  filled: true,
+                  border: InputBorder.none,
+                  hintText: "Find your music",
+                  hintStyle: TextStyle(color: Colors.black, fontSize: 25)),
+            ),
+          ),
+          Expanded(
+            child: GridView.count(
+              primary: false,
+              padding: EdgeInsets.all(10),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 2,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
+                    child: Text(
+                      "Pop Music",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                          Colors.deepOrange,
+                          Colors.deepOrange.withOpacity(0.1)
+                        ])),
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
+                    child: Text(
+                      "Rock",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                          Colors.deepPurple,
+                          Colors.deepPurple.withOpacity(0.1)
+                        ])),
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
+                    child: Text(
+                      "Hip Hop",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                          Colors.blue,
+                          Colors.blue.withOpacity(0.1)
+                        ])),
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
+                    child: Text(
+                      "Jazz",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                          Colors.amber,
+                          Colors.amber.withOpacity(0.1)
+                        ])),
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
+                    child: Text(
+                      "House",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                          Colors.green,
+                          Colors.green.withOpacity(0.1)
+                        ])),
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
+                    child: Text(
+                      "Reggae",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [Colors.red, Colors.red.withOpacity(0.1)])),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
